54f4ba1157cbb8f7fb92a4cf2251830fa2a40e18
Reinaldo Ribeiro <lunajf18@gmail.com>
2024-11-14 22:39:27 -0300

    Criando endpoint de reordenação da lista

--- dslist/src/main/java/com/reinaldo/dslist/controller/GameListController.java
+++ dslist/src/main/java/com/reinaldo/dslist/controller/GameListController.java
@@ -2,14 +2,12 @@ package com.reinaldo.dslist.controller;
 
 import com.reinaldo.dslist.dto.GameListDTO;
 import com.reinaldo.dslist.dto.GameMinDTO;
+import com.reinaldo.dslist.dto.ReplacementDTO;
 import com.reinaldo.dslist.projections.GameMinProjection;
 import com.reinaldo.dslist.services.GameListService;
 import com.reinaldo.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +30,10 @@ public class GameListController {
     public List<GameMinDTO> findByList(@PathVariable Long listId) {
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
